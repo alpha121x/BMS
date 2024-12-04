06810896e4932040622a51f115c5e09854f691fc
Abbasch <109143082+alpha121x@users.noreply.github.com>
2024-12-04 14:17:01 +0500

    Update Dashboard.jsx

--- frontend/src/Dashboard.jsx
+++ frontend/src/Dashboard.jsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import Header from './components/Header';
-import DashboardMain from './components/DashboardMain';
-import Footer from './components/Footer';
-import './index.css';
+import React from "react";
+import Header from "./components/Header";
+import DashboardMain from "./components/DashboardMain";
+import Footer from "./components/Footer";
+import "./index.css";
 
 const Dashboard = () => {
+  /* 
+  Designed and Developed By: Abbas Ch.
+  React and Node.js Developer
+  abbasshakor0123@gmail.com
+  Date: 11/21/2024
+*/
 
-    return (
-        <div className="flex flex-col min-h-screen">
-            <div className='mb-1'>
-                <Header />
-            </div>
+  return (
+    <div className="flex flex-col min-h-screen">
+      <div className="mb-1">
+        <Header />
+      </div>
 
-            <main className="flex-grow p-1">
-                <DashboardMain />
-            </main>
+      <main className="flex-grow p-1">
+        <DashboardMain />
+      </main>
 
-            <Footer className="mt-1"  />
-        </div>
-    );
+      <Footer className="mt-1" />
+    </div>
+  );
 };
 
 export default Dashboard;
