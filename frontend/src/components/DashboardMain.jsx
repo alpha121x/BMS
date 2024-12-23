122b5099bfe92cdbabe4f5210034ed4d821bc5ef
Abbasch <109143082+alpha121x@users.noreply.github.com>
2024-12-23 16:41:01 +0500

    updates

--- frontend/src/components/DashboardMain.jsx
+++ frontend/src/components/DashboardMain.jsx
@@ -206,7 +206,10 @@ const DashboardMain = () => {
           >
             <div className="card-body pb-0">
               <h2 className="text-xl font-semibold mb-4">Map</h2>
-              <Map />
+              <Map
+              selectedDistrict={selectedDistrict}
+              selectedZone={selectedZone}
+              />
             </div>
           </div>
         </div>
