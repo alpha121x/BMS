2691c76c50591e8134f02c5146b1c0eb31d9f006
Abbasch <109143082+alpha121x@users.noreply.github.com>
2025-01-02 12:35:50 +0500

    Update EditForm.jsx

--- frontend/src/components/EditForm.jsx
+++ frontend/src/components/EditForm.jsx
@@ -76,14 +76,28 @@ const EditForm = () => {
     }));
   };
 
-  if (!bridgeData) {
-    return (
-      <div className="container my-5">
-        <p>Loading bridge data...</p>
-      </div>
-    );
-  }
-
+      if (!bridgeData) {
+        return (
+          <div
+            className="loader"
+            style={{
+              border: "8px solid #f3f3f3",
+              borderTop: "8px solid #3498db",
+              borderRadius: "50%",
+              width: "80px",
+              height: "80px",
+              animation: "spin 1s linear infinite",
+              margin: "auto",
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              zIndex: "999",
+            }}
+          />
+        );
+      }
+      
   return (
     <div className="container d-flex justify-content-center align-items-center min-vh-100">
       <div
