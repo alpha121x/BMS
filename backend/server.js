ffa8ad0fcf00d36ef1a710aac26ae6efd49bc525
Abbasch <109143082+alpha121x@users.noreply.github.com>
2025-01-15 14:40:34 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const jwt = require("jsonwebtoken");
 const multer = require("multer");
 const path = require("path");
+const fs = require('fs');
 const cors = require("cors");
 const { Pool } = require("pg");
 require("dotenv").config();
