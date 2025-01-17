79ed3e08637d12590951ae87317d3549a0fb0d0f
Abbasch <109143082+alpha121x@users.noreply.github.com>
2025-01-17 11:29:52 +0500

    Update DashboardMain.jsx

--- frontend/src/components/DashboardMain.jsx
+++ frontend/src/components/DashboardMain.jsx
@@ -8,7 +8,6 @@ import {
 } from "@heroicons/react/24/outline";
 import FilterComponent from "./FilterComponent";
 import Map from "./Map";
-import GraphComponent from "./GraphComponent";
 import BridgesListDashboard from "./BirdgesListDashboard";
 
 const DashboardMain = () => {
