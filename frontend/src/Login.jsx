e5e3b010d897187fdf2052452bc54e49f553984b
Abbasch <109143082+alpha121x@users.noreply.github.com>
2025-03-25 15:32:56 +0500

    Update Login.jsx

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser, faLock, faSpinner } from "@fortawesome/free-solid-svg-icons"; 
+import { faUser, faLock, faSpinner } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
 import cnw_2 from "/cnw_2.png";
-import { BASE_URL } from './components/config.jsx';
+import { BASE_URL } from "./components/config.jsx";
 
 const Login = () => {
   const [username, setUsername] = useState("");
@@ -14,7 +14,7 @@ const Login = () => {
 
   const handleLogin = async () => {
     setLoading(true);
-    setError(""); 
+    setError("");
     try {
       const response = await fetch(`${BASE_URL}/api/login`, {
         method: "POST",
@@ -27,9 +27,9 @@ const Login = () => {
       const data = await response.json();
 
       if (!response.ok) {
-        setError(data.message); 
+        setError(data.message);
         console.error("Login failed:", data.message);
-        setLoading(false); 
+        setLoading(false);
         return;
       }
 
@@ -51,7 +51,7 @@ const Login = () => {
       console.error("Login error:", error);
       setError("An error occurred. Please try again.");
     } finally {
-      setLoading(false); 
+      setLoading(false);
     }
   };
 
@@ -63,7 +63,15 @@ const Login = () => {
   };
 
   return (
-    <div style={{ height: "100vh",backgroundImage: "url('/bg11_new.png')",backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition: "center"}}>
+    <div
+      style={{
+        height: "100vh",
+        backgroundImage: "url('/bg11_new.png')",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
       {/* Main Container */}
       <div className="container">
         <header>
@@ -71,29 +79,45 @@ const Login = () => {
             <div className="container mx-auto">
               {/* Navbar Brand */}
               <a href="/" className="navbar-brand">
-                <img src={cnw_2} alt="logo" style={{width:"300px"}} />
+                <img src={cnw_2} alt="logo" style={{ width: "300px" }} />
               </a>
             </div>
           </nav>
         </header>
 
         {/* Login Card */}
-        <div className="d-flex justify-content-around align-items-center shadow-lg p-8 rounded bg-[#f3f3f3]" style={{height:"450px"}}>
+        <div
+          className="d-flex justify-content-around align-items-center shadow-lg p-8 rounded bg-[#f3f3f3]"
+          style={{ height: "450px" }}
+        >
           {/* Left Column - Image and Title */}
           <div className="text-center lg:text-left lg:w-1/3 mb-6 lg:mb-0">
             {/*<img src={bmslogo} alt="Agriculture Graduates" className="w-100 h-100 mx-auto lg:mx-0 rounded-lg opacity-90"/>*/}
             <div>
-              <h5 className="text-uppercase fw-bold text-[#005D7F] mb-3">Bridge Management Systems</h5>
-              <p className="fs-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, aliquid autem deserunt earum eligendi in ipsa itaque minus nesciunt officia perspiciatis quos reiciendis repellendus sunt suscipit tempore unde voluptate voluptates!</p>
-              <p className="mt-5 fs-6">Designed and Developed by <a href="" className="text-[#005D7F]">The Urban Unit</a></p>
+              <h5 className="text-uppercase fw-bold text-[#005D7F] mb-3">
+                Bridge Management Systems
+              </h5>
+              <p className="fs-6">
+                The BMS Software is a specialized tool developed to support the
+                comprehensive management of bridge infrastructure.It is specifically designed to help engineers, 
+                inspectors, and maintenance planners streamline the following critical activities: 
+              </p>
+              
+              <p className="mt-5 fs-6">
+                Designed and Developed by{" "}
+                <a href="" className="text-[#005D7F]">
+                  The Urban Unit
+                </a>
+              </p>
             </div>
-
           </div>
 
           {/* Right Column - Login Form */}
           <div className="lg:w-1/3">
             <div className="text-center lg:text-left mb-4">
-              <h4 className="text-xl font-bold text-gray-700">Dashboard Login</h4>
+              <h4 className="text-xl font-bold text-gray-700">
+                Dashboard Login
+              </h4>
               <p>Enter username and password to login</p>
             </div>
             {/* Login Fields */}
@@ -132,7 +156,7 @@ const Login = () => {
               <button
                 onClick={handleLogin}
                 className="w-full py-2 bg-[#005D7F] text-white font-semibold rounded-lg hover:bg-[#00A2BE] transition flex items-center justify-center"
-                disabled={loading} 
+                disabled={loading}
               >
                 {loading ? (
                   <FontAwesomeIcon icon={faSpinner} spin className="mr-2" />
