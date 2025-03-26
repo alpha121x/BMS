acf5ee2950422bbe4cee6f686bc0fc19d4eb2899
Abbasch <109143082+alpha121x@users.noreply.github.com>
2025-03-26 11:40:40 +0500

    Update TopCardDashboard.jsx

--- frontend/src/components/TopCardDashboard.jsx
+++ frontend/src/components/TopCardDashboard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const TopCardDashboard = ({ totalLabel, totalValue, color, items }) => {
     const addCommas = (x) => {
-        if (!x) return "N/A";
+        if (!x) return "0";
         var parts = x.toString().split(".");
         parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
         return parts.join(".");
