732604b0c12a4d9db1395dae53b62462de788cd1
Abbasch <109143082+alpha121x@users.noreply.github.com>
2025-04-14 13:45:08 +0500

    Updatess

--- frontend/src/components/CostEstimation.jsx
+++ frontend/src/components/CostEstimation.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Header from "./Header";
+import Footer from "./Footer";
 
 const CostEstimation = () => {
   return (
     <div>
+      <Header />
       Cost Estimation
+      <Footer />
     </div>
   )
 }
