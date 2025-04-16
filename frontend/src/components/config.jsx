69e3295542fcc5dd30fd7184975150213180cec8
Abbasch <109143082+alpha121x@users.noreply.github.com>
2025-04-16 11:25:42 +0500

    Update config.jsx

--- frontend/src/components/config.jsx
+++ frontend/src/components/config.jsx
@@ -1,2 +1,2 @@
-// export const BASE_URL = "http://localhost:8081";
-export const BASE_URL = "http://cnw.urbanunit.gov.pk:3037";
+export const BASE_URL = "http://localhost:8081";
+// export const BASE_URL = "http://cnw.urbanunit.gov.pk:3037";
