bec0bf383a7eac1219446ae44e23d42c804d5a02
Abbasch <109143082+alpha121x@users.noreply.github.com>
2025-06-03 13:29:15 +0500

    Update EvaluationMainRams.jsx

--- frontend/src/components/EvaluationMainRams.jsx
+++ frontend/src/components/EvaluationMainRams.jsx
@@ -173,7 +173,7 @@ const EvaluationMainRams = () => {
                     : "bg-[#88B9B8] text-white hover:bg-[#005D7F]"
                 }`}
               >
-                Approved/Pending By Consultant
+                Bridges Status Summary
               </button>
             </div>
 
